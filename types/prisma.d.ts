a59003cee1ff3bf0a1d457f413b756b978ab40c0
4shk4n-code <4shk4n@gmail.com>
2025-12-11 13:15:30 +0400

    Simplify fix-prisma script and update type declarations path

--- types/prisma.d.ts
+++ types/prisma.d.ts
@@ -1,6 +1,6 @@
 // Type declaration for Prisma Client with custom output path
 declare module '@prisma/client' {
-  export { PrismaClient } from '../node_modules/.prisma/client';
-  export type * from '../node_modules/.prisma/client';
+  export { PrismaClient } from '../.prisma/client';
+  export type * from '../.prisma/client';
 }
 
