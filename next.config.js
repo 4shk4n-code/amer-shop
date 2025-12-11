26b5f14d75bfb1afa1eb8971757c56cb95acc7ea
4shk4n-code <4shk4n@gmail.com>
2025-12-11 12:59:30 +0400

    Fix Prisma Client import with webpack alias

--- next.config.js
+++ next.config.js
@@ -16,6 +16,13 @@ const nextConfig = {
       },
     ],
   },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@prisma/client': require('path').resolve(__dirname, 'node_modules/.prisma/client'),
+    };
+    return config;
+  },
 };
 
 module.exports = nextConfig;
