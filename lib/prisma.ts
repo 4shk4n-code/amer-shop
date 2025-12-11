aa9c4a4295cd22738879c1930a202030e30fe062
4shk4n-code <4shk4n@gmail.com>
2025-12-11 13:02:40 +0400

    Add TypeScript types for require-based Prisma import

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,7 +1,10 @@
+// @ts-ignore - Prisma Client is generated to custom location
 const { PrismaClient } = require('../node_modules/.prisma/client');
 
+type PrismaClientType = typeof PrismaClient;
+
 const globalForPrisma = globalThis as unknown as {
-  prisma: typeof PrismaClient | undefined
+  prisma: InstanceType<PrismaClientType> | undefined
 }
 
 export const prisma = globalForPrisma.prisma ?? new PrismaClient()
