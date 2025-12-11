185a1bf491e861455343c48edcf5c82574188d48
4shk4n-code <4shk4n@gmail.com>
2025-12-11 13:00:54 +0400

    Add TypeScript path mapping for Prisma Client

--- types/prisma.d.ts
+++ types/prisma.d.ts
@@ -1,4 +1,6 @@
+// Type declaration for Prisma Client with custom output path
 declare module '@prisma/client' {
-  export * from '../node_modules/.prisma/client';
+  export { PrismaClient } from '../node_modules/.prisma/client';
+  export type * from '../node_modules/.prisma/client';
 }
 
